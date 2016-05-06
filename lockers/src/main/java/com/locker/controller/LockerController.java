8ecfb42bcc5d536ed1cddcac11e10191c8ffda1f
Moenish Baladien <Moenish.Baladien@hva.nl>
2016-05-06 20:47:03 +0200

    sort functie in locker lijst

--- lockers/src/main/java/com/locker/controller/LockerController.java
+++ lockers/src/main/java/com/locker/controller/LockerController.java
@@ -30,8 +30,9 @@ public class LockerController {
 
     @RequestMapping(value = "/locker/{id}",method = RequestMethod.GET)
     public ModelAndView lockerWithId(@PathVariable Long id) {
-        ModelAndView model = new ModelAndView("");
+        ModelAndView model = new ModelAndView("view");
         LockerEntity locker = lockerService.findLockerById(id);
+        model.addObject("locker",locker);
         return model;
     }
 
